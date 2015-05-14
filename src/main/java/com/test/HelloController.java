2b9132ef22f2c6a2c9545a6f565d516fd55c26cf
Arpit Aggarwal <aggarwalarpit.89@gmail.com>
2015-05-14 17:58:39 +0200

    Initial Commit

---
+++ src/main/java/com/test/HelloController.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/welcome")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Spring MVC Hello World");
+		return "hello";
+	}
+}
